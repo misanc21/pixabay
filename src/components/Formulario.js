f2cebfbd9aba3d64ff681ff3d453178258f9aaf0
misanc21 <misanc21@gmail.com>
2020-09-06 22:19:47 -0500

    validar form

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Error from './Error'
+
 const Formulario = () => {
-    return (  
-        <form>
+
+    const [termino, setTermino] = useState('')
+    const [error, setError] = useState(false)
+
+    const buscarimagenes = e => {
+        e.preventDefault();
+
+        if(termino.trim() === ''){
+            setError(true)
+            return
+        }
+    }
+
+    return (
+        <form
+            onSubmit = {buscarimagenes}
+        >
             <div className="row">
                 <div className="form-group col-md-8">
-                    <input 
+                    <input
                         type="text"
                         className="form-control form-control-lg"
                         placeholder="Busca una imagen"
+                        onChange= {e => setTermino(e.target.value)}
+                        value={termino}
                     />
                 </div>
                 <div className="form-group col-md-4">
@@ -19,8 +39,9 @@ const Formulario = () => {
                     </button>
                 </div>
             </div>
+            {error ? <Error mensaje="agrega un termino de busqueda"/> : null}
         </form>
     )
 }
- 
+
 export default Formulario;

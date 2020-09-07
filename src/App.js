4a9428e939ec7edccfc552d222a4c3c6769e53f4
misanc21 <misanc21@gmail.com>
2020-09-06 22:13:06 -0500

    formulario html

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Formulario from './components/Formulario'
 
 function App() {
   return (
     <div className="container">
       <div className="jumbotron">
         <p className="lead text-center">Buscador de imagenes</p>
+        <Formulario/>
       </div>
     </div>
   );
